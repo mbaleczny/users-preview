06cc6c448573309b59cc12ecf7699e6359a48e56
Mariusz Baleczny <mariuszbaleczny@gmail.com>
2019-05-10 22:29:25 +0200

    Create project

---
+++ app/src/main/java/com/mbaleczny/userspreview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mbaleczny.userspreview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
